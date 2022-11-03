5a47f80c26be497da946992a1b190f0044f7206e
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-03 14:40:05 +0300

    Find max value in array

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,17 @@
 package com.epam.rd.autotasks.max;
 
 public class MaxMethod {
+    public static void main(String[] args) {
+        int[] array = new int[] {-2, 0, 10, 5};
+        System.out.println(max(array));
+    }
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = values[0];
+        for (int value : values) {
+            if (max < value) {
+                max = value;
+            }
+        }
+        return max;
     }
 }
